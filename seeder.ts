5e8dd90d4917c8c185233f14d8f63361d86f0392
Artem Telesh <decarea@yahoo.com>
2023-08-09 22:14:31 -0400

    added check for return value to determine if seeder run was successfull

--- seeder.ts
+++ seeder.ts
@@ -2,10 +2,16 @@ import { seeder } from './umzug'
 
 if (require.main === module) {
   seeder.runAsCLI()
-    .then(() => {
-      console.log('Seeder complted successfully')
-      // exit with success
-      process.exit(0)
+    .then((success: boolean) => {
+      if (success) {
+        console.log('Seeder complted successfully')
+        // exit with success
+        process.exit(0)
+      } else {
+        console.log('Seeder finished with errors')
+        // exit with failure
+        process.exit(1)
+      }
     })
     .catch((err) => {
       console.log('Seeder has failed')
