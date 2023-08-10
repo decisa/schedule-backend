ea1aa54d584e554796b55d96c157fde75ed887b8
Artem Telesh <decarea@yahoo.com>
2023-08-09 22:13:46 -0400

    added check for return value to determine if migration was a success

--- migrator.ts
+++ migrator.ts
@@ -2,10 +2,15 @@ import { migrator } from './umzug'
 
 if (require.main === module) {
   migrator.runAsCLI()
-    .then(() => {
-      console.log('Migrator complted successfully')
-      // exit with success
-      process.exit(0)
+    .then((success: boolean) => {
+      if (success) {
+        console.log('migration completed successfully')
+        // exit with success
+        process.exit(0)
+      }
+      console.log('migration finished with errors')
+      // exit with failure
+      process.exit(1)
     })
     .catch((err) => {
       console.log('Migrator has failed')
