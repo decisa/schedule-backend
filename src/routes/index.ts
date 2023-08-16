893a87d429966979e4b385d9eafa5dd6a65a8331
Art Telesh <decarea@yahoo.com>
2023-08-16 10:58:00 -0400

    replaced http-proxy-middleware with custom axios implementation to proxy requests to magento and bypass cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express'
-import { PassThrough } from 'stream'
-import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware'
+import axios from 'axios'
+// import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware'
 import commentRouter from './comments/commentRoutes'
 import customerRouter from './customers/customerRoutes'
 import addressRouter from './addresses/addressRoutes'
@@ -14,10 +15,6 @@ import deliveryMethodRouter from './deliveryMethods/deliveryMethodRoutes'
 import purchaseOrderRouter from './purchaseOrders/purchaseOrderRoutes'
 
 const rootRouter = express.Router()
-// rootRouter.use((req, res, next) => {
-//   console.log(`Received ${req.method} request for ${req.url}`)
-//   next()
-// })
 
 rootRouter.get('/', (req, res) => {
   res.send('hi')
@@ -35,28 +32,66 @@ rootRouter.use('/order', orderRouter)
 rootRouter.use('/deliverymethod', deliveryMethodRouter)
 rootRouter.use('/purchaseorder', purchaseOrderRouter)
 
-// create proxy to forward requests to magento
+// create proxy to forward requests to magento:
 rootRouter.use('/2031360', (req, res, next) => {
   // Set CORS headers
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-
-  // Respond to preflight requests
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,HEAD')
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept')
   if (req.method === 'OPTIONS') {
     // console.log('sending 200 for options request')
     res.sendStatus(200)
-  } else {
     next()
+    return
   }
-}, createProxyMiddleware({
-  target: 'https://www.roomservice360.com', // the target host
-  changeOrigin: true, // needed for virtual hosted sites
-  secure: true,
-  pathRewrite: {
-    '^/2031360': '', // rewrite path
-  },
-  logLevel: 'info',
-}))
+
+  const newHeaders = {
+    ...req.headers,
+  }
+  delete newHeaders.host
+  delete newHeaders.origin
+  delete newHeaders.referer
+  // newHeaders['content-length']
+  delete newHeaders.cookie
+  // console.log('HEADERS:', newHeaders)
+  axios({
+    method: req.method,
+    url: `https://www.roomservice360.com${req.originalUrl.replace('/2031360', '')}`,
+    headers: {
+      ...newHeaders,
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    data: req.body,
+  }).then(({ data }) => {
+    res.json(data)
+  }).catch((error) => {
+    console.error('error:', error)
+    res.status(500).send('Proxy error')
+  })
+})
+
+// create proxy to forward requests to magento (using http-proxy-middleware):
+// rootRouter.use('/2031360', (req, res, next) => {
+//   // Set CORS headers
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,HEAD')
+//   res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept')
+
+//   // Respond to preflight requests
+//   if (req.method === 'OPTIONS') {
+//     // console.log('sending 200 for options request')
+//     res.sendStatus(200)
+//   } else {
+//     next()
+//   }
+// }, createProxyMiddleware({
+//   target: 'https://www.roomservice360.com', // the target host
+//   changeOrigin: true, // needed for virtual hosted sites
+//   secure: true,
+//   pathRewrite: {
+//     '^/2031360': '', // rewrite path
+//   },
+//   logLevel: 'info',
+// }))
 
 export default rootRouter
