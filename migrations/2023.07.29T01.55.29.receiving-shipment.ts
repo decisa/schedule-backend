91ae449b3c5339542a3ceb1ed86b73daca442374
Art Telesh <decarea@yahoo.com>
2023-11-18 05:55:55 -0500

    added constraint to limit unique tracking number per carrier

--- migrations/2023.07.29T01.55.29.receiving-shipment.ts
+++ migrations/2023.07.29T01.55.29.receiving-shipment.ts
@@ -2,36 +2,68 @@ import { DataTypes } from 'sequelize'
 // import { MigrationFn } from 'umzug';
 import { Migration } from '../umzug'
 
-export const up: Migration = async ({ context: queryIterface }) => {
-  await queryIterface.createTable('Shipments', {
-    id: {
-      type: DataTypes.INTEGER,
-      unique: true,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    trackingNumber: DataTypes.STRING,
-    eta: DataTypes.DATE,
-    dateShipped: DataTypes.DATE,
-
-    // foreign keys:
-    carrierId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'Carriers',
-        key: 'id',
+export const up: Migration = async ({ context: queryInterface }) => {
+  const transaction = await queryInterface.sequelize.transaction()
+  try {
+    await queryInterface.createTable('Shipments', {
+      id: {
+        type: DataTypes.INTEGER,
+        unique: true,
+        autoIncrement: true,
+        primaryKey: true,
       },
-      // restrict deletion of carrier if shipments exist
-      onDelete: 'RESTRICT',
-      onUpdate: 'CASCADE',
-    },
+      trackingNumber: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      eta: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      dateShipped: {
+        type: DataTypes.DATE,
+        allowNull: false, // now that there are no more NULLs, this won't cause an error
+        defaultValue: DataTypes.NOW,
+      },
+      // foreign keys:
+      carrierId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Carriers',
+          key: 'id',
+        },
+        allowNull: false,
+        // restrict deletion of carrier if shipments exist
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE',
+      },
+      // timestamps:
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+    })
 
-    // timestamps:
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE,
-  })
+    await queryInterface.addConstraint('Shipments', {
+      fields: ['trackingNumber', 'carrierId'],
+      type: 'unique',
+      name: 'trackingNumber_carrierId_constraint',
+    })
+
+    await transaction.commit()
+  } catch (error) {
+    await transaction.rollback()
+    throw error
+  }
 }
 
-export const down: Migration = async ({ context: queryIterface }) => {
-  await queryIterface.dropTable('Shipments')
+export const down: Migration = async ({ context: queryInterface }) => {
+  const transaction = await queryInterface.sequelize.transaction()
+  try {
+    await queryInterface.removeConstraint('Shipments', 'trackingNumber_carrierId_constraint', { transaction })
+    await queryInterface.dropTable('Shipments', { transaction })
+
+    await transaction.commit()
+  } catch (error) {
+    await transaction.rollback()
+    throw error
+  }
 }
