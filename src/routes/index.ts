03f26894fedc4e113357523ac31ffc6f94b735cb
Art Telesh <decarea@yahoo.com>
2024-05-11 11:00:57 -0400

    added proxy for dev server

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -21,6 +21,7 @@ import vehicleRouter from './vehicles/vehicleRoutes'
 import driverRouter from './driver/driverRoutes'
 import deliveryStopRouter from './deliveryStop/deliveryStopRoutes'
 import tripRouter from './trip/tripRoutes'
+import { printRedLine } from '../utils/utils'
 
 const rootRouter = express.Router()
 
@@ -50,6 +51,71 @@ rootRouter.use('/trip', tripRouter)
 
 // create proxy to forward requests to magento:
 rootRouter.use('/2031360', (req, res, next) => {
+  // printRedLine()
+  // console.log('requesting data through proxy')
+  // Set CORS headers
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,HEAD')
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept')
+  if (req.method === 'OPTIONS') {
+    // console.log('sending 200 for options request')
+    res.sendStatus(200)
+    next()
+    return
+  }
+
+  // console.log('non options request')
+
+  const newHeaders = {
+    ...req.headers,
+    // custom user agent
+    'user-agent': 'roomservice360/decarea',
+  }
+  delete newHeaders.host
+  delete newHeaders.origin
+  delete newHeaders.referer
+  // newHeaders['content-length']
+  delete newHeaders.cookie
+  // console.log('HEADERS:', newHeaders)
+
+  // for some reason node 20.12 is encoding empty body as '{}' instead of undefined
+  // so we need to set it to undefined
+  let { body } = req
+  // console.log('!!req.body size:', (req.body as string)?.length)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  if (req.body && typeof req.body === 'object' && Object.keys(req.body).length === 0) {
+    body = undefined
+  }
+
+  axios({
+    method: req.method,
+    url: `https://www.roomservice360.com${req.originalUrl.replace('/2031360', '')}`,
+    headers: {
+      ...newHeaders,
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    data: body,
+  }).then((response) => {
+    const { data } = response
+    // console.log('response:', response)
+    res.json(data)
+  }).catch((error) => {
+    console.error('error:', error)
+    // console.error('error:')
+
+    // handle unauthorized error
+    if (error && error instanceof AxiosError && error.response && error.response.status === 401) {
+      res.status(401).send({
+        status: error.response.status,
+        statusText: error.response.statusText,
+        ...error.response.data,
+      })
+    } else res.status(500).send('Proxy error')
+  })
+})
+
+// create proxy to forward requests to dev2 magento:
+rootRouter.use('/2031361', (req, res, next) => {
   // Set CORS headers
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,HEAD')
@@ -83,7 +149,7 @@ rootRouter.use('/2031360', (req, res, next) => {
 
   axios({
     method: req.method,
-    url: `https://www.roomservice360.com${req.originalUrl.replace('/2031360', '')}`,
+    url: `https://dev2.roomservice360.com${req.originalUrl.replace('/2031361', '')}`,
     headers: {
       ...newHeaders,
     },
